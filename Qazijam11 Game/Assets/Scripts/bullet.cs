a60de20ad323c4a9e0eefcca53973d3e024473ae
YkorLP <34199120+YkorLP@users.noreply.github.com>
2018-06-01 16:28:22 -0600

    Shooting

---
+++ Qazijam11 Game/Assets/Scripts/bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    Rigidbody2D rigidBody;
+    public float speed;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rigidBody.AddForce(gameObject.transform.up * speed * 1);
+    }
+}
---
+++ Qazijam11 Game/Assets/Scripts/playerShoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerShoot : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public GameObject firePoint;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet, firePoint.transform.position, firePoint.transform.rotation);
+        }
+    }
+}
