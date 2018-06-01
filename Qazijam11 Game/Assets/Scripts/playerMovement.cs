5fc1eaa825134e91f3a91daf4aab59d46b4a70c6
YkorLP <34199120+YkorLP@users.noreply.github.com>
2018-06-01 15:39:03 -0600

    Test

---
+++ Qazijam11 Game/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey("w"))
+        {
+            transform.position += Vector3.up * speed / 10;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position += Vector3.down * speed / 10;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position += Vector3.left * speed / 10;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += Vector3.right * speed / 10;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        rigidBody.angularVelocity = 0;
+    }
+}
