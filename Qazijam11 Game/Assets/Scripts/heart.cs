ae667edcb60ae5e7e8a245dfba8a03621d4b4a38
YkorLP <34199120+YkorLP@users.noreply.github.com>
2018-06-01 20:30:22 -0600

    hate my life

---
+++ Qazijam11 Game/Assets/Scripts/heart.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heart : MonoBehaviour
+{
+
+    public bool full;
+    public bool empty;
+}
---
+++ Qazijam11 Game/Assets/Scripts/playerStats.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerStats : MonoBehaviour
+{
+    public bool takeDamageDebug;
+
+    public int hearts;
+
+    public GameObject heart1;
+    public GameObject heart2;
+    public GameObject heart3;
+
+    void TakeDamage ()
+    {
+        hearts-= 1;
+
+    }
+
+    void Update() // hate my life
+    {
+        if (hearts == 2)
+        {
+            heart3.GetComponent<Animation>().Play("LoseHealth");
+        }
+        if (hearts == 1)
+        {
+            heart2.GetComponent<Animation>().Play("LoseHealth");
+        }
+    }
+
+    /*void FindFullHeart ()
+    {
+        if (heart1.GetComponent<heart>().full != false) //Finds full heart
+        {
+            if (heart2.GetComponent<heart>().full != false)
+            {
+                if (heart3.GetComponent<heart>().full != false)
+                {
+                    //Die
+                }
+                else
+                {
+                    GameObject fullHeart;
+                    fullHeart = heart3.gameObject;
+                    Animator animator = fullHeart.GetComponent<Animator>();
+                    animator.SetTrigger("LoseHealth");
+                    return;
+                }
+            }
+            else
+            {
+                GameObject fullHeart;
+                fullHeart = heart2.gameObject;
+                Animator animator = fullHeart.GetComponent<Animator>();
+                animator.SetTrigger("LoseHealth");
+                return;
+            }
+        }
+        else
+        {
+            GameObject fullHeart;
+            fullHeart = heart1.gameObject;
+            Animator animator = fullHeart.GetComponent<Animator>();
+            animator.SetTrigger("LoseHealth");
+            return;
+        }
+
+       
+        
+    }*/
+}
