cbcefdfc1fedb262774a46ec6f3975e938794810
YkorLP <34199120+YkorLP@users.noreply.github.com>
2018-06-01 18:48:58 -0600

    Camera Movement

---
+++ Qazijam11 Game/Assets/Scripts/enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    private void FixedUpdate()
+    {
+        var playerPosition = player.transform.position;
+        Quaternion rot = Quaternion.LookRotation(transform.position - playerPosition, Vector3.forward);
+
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+    }
+}
